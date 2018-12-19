9c2a30399f3d281696b7a624c072dc1680fa276f
Inam2000 <45816632+Inam2000@users.noreply.github.com>
2018-12-19 11:56:08 +0100

    Update execute.php

--- marzo8/execute.php
+++ marzo8/execute.php
@@ -55,6 +55,12 @@
 		$parameters = array('chat_id' => $chatId, "text" => $text);
 		}
                 if($text=="foto"||$text=="/foto"){
+			$foto[0]"foto-png";
+			$foto[1]"foto1.png";
+			$foto[2]"foto2.png";
+			$num=rand(0,2)
+			
+			
 		sendFoto($chatId, "foto.png", false, "La mia foto", $api);
 		}
 			
